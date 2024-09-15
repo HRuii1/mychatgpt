d5add6ff875bb868cde8602b26b5b04e2c6f60ed
HRuii1 <liuhuarui2004@gmail.com>
2024-09-15 15:57:09 -0400

    image uploading

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,23 +1,38 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './newPrompt.css'
+import Upload from '../upload/Upload';
+import { IKImage } from 'imagekitio-react';
 
 const NewPrompt = () => {
 
+    const[img, setImg] = useState({
+        isLoading:false,
+        error:"",
+        dbData:{}
+    })
+
     const endRef = useRef(null);
 
-    useEffect(()=>{
-        endRef.current.scrollIntoView({behavior: "smooth"});
-    }, [])
+    useEffect(() => {
+        endRef.current.scrollIntoView({ behavior: "smooth" });
+      }, [img.dbData]);
 
     return (
         <div className='newPrompt'>
             {/* add new chat here */}
+            {img.isLoading && <div className=''>Loading...</div>}
+            {img.dbData?.filePath && (
+                <IKImage
+                urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                path={img.dbData?.filePath}
+                width="380"
+                transformation={[{ width: 380 }]}
+                />
+            )}
             {/* mark the end of this chat and endRef will be trigger to automatically scroll down the chat one the component mounts */}
             <div className="endChat" ref={endRef}></div>
             <form className="newForm">
-                <label htmlFor="file">
-                    <img src="/attachment.png" alt="" />
-                </label>
+                <Upload setImg={setImg}/>
                 {/* use id from htmlFor, so the input is hidden*/}
                 <input id="file" type="file" multiple={false} hidden/>
                 <input type="text" placeholder='Ask anything...'/>
